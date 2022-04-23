300ebc66ab416dc4bc1cbded42cac0b59773554d
sayole <sylee8191@gmail.com>
2022-04-23 19:02:35 +0900

    images sources updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:job_doc/pages/login/1_onboarding.dart';
+import 'package:job_doc/pages/login/2_sign_in.dart';
 import 'package:job_doc/pages/main_home.dart';
 import 'package:job_doc/palette.dart';
 import 'package:job_doc/services/auth_service.dart';
@@ -27,11 +28,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: Palette.background),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
         fontFamily: 'Pretendard',
         primaryColor: Palette.background,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: OnBoarding(),
+      home: SignIn(),
     );
   }
 }
--- lib/pages/login/2_sign_in.dart
+++ lib/pages/login/2_sign_in.dart
@@ -1,1 +1,94 @@
 //회원가입 페이지
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                  Text(
+                    "빠르게 이직 컨설팅 받고",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    "퀀텀 점프하기",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  Image.asset('assets/sign_in_image.png'),
+                ],
+              ),
+              Positioned(
+                bottom: 80,
+                child: Column(
+                  children: [
+                    SignInButton(
+                      Buttons.Google,
+                      text: '구글로 로그인하기',
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
+                      children: [
+                        TextSpan(text: "회원가입 시 "),
+                        TextSpan(
+                          text: '개인정보 처리 방침',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                          ),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                        ),
+                        TextSpan(text: '을 읽었으며,\n  '),
+                        TextSpan(
+                          text: '이용약관',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                          ),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                        ),
+                        TextSpan(text: "에 동의하신 것으로 간주합니다."),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
