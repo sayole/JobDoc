18fc376dc42225b97dfa47e0ed5c0866b71cba8a
sayole <sylee8191@gmail.com>
2022-04-23 15:27:30 +0900

    updated Font, Palette, icons

--- lib/pages/main_home.dart
+++ lib/pages/main_home.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/button_list.dart';
-import 'package:flutter_signin_button/button_view.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:job_doc/models/user_data.dart';
 
@@ -12,47 +10,15 @@ class HomePage extends StatelessWidget {
   GoogleSignIn googleSignIn = GoogleSignIn();
   @override
   Widget build(BuildContext context) {
-    Future<void> onPressed() async {
-      GoogleSignInAccount? account = await googleSignIn.signIn();
-      GoogleSignInAuthentication? authentication =
-          await account?.authentication;
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("홈페이지"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Text(
-                user.name,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-            user.careerCompany.isEmpty
-                ? Text('')
-                : Center(
-                    child: Text(
-                      user.careerCompany,
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ),
-            SignInButton(
-              Buttons.Google,
-              onPressed: onPressed,
-              text: "Sign up with Google",
-            )
-          ],
+        appBar: AppBar(
+          title: Text(
+            "홈페이지",
+            style: TextStyle(fontFamily: 'Pretendard'),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: onPressed,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Text("hello"),
+        ));
   }
 }
